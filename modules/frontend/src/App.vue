e914db0a598ba4bd85c8fc44f7a3b36280cd6ddf
alex1431999 <alexhaller99@yahoo.de>
2024-05-14 20:17:50 +0200

    chore: fix linting

--- modules/frontend/src/App.vue
+++ modules/frontend/src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <RouterView />
+    <RouterView/>
 </template>
 
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import {RouterView} from 'vue-router'
 </script>
 
