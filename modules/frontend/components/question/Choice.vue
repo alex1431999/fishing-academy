14ff400c4640d5eb2bc6ce546210cbc4cba055f6
alex1431999 <alexhaller99@yahoo.de>
2024-05-14 21:05:55 +0200

    feat: render question with choices

---
+++ modules/frontend/components/question/Choice.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-btn>{{ choice.description }}</v-btn>
+</template>
+
+<script setup lang="ts">
+import type {Choice} from "fishing-academy-types"
+
+const {choice} = defineProps<{ choice: Choice }>()
+</script>
--- modules/frontend/components/question/QuestionCard.vue
+++ modules/frontend/components/question/QuestionCard.vue
@@ -1,11 +1,16 @@
 <template>
     <v-card>
-        {{ question }}
+        <v-card-text>
+            {{ question.description }}
+
+            <Choice v-for="choice in question.choices" :choice="choice"></Choice>
+        </v-card-text>
     </v-card>
 </template>
 
 <script setup lang="ts">
 import type {Question} from 'fishing-academy-types'
+import Choice from "~/components/question/Choice.vue";
 
 const {question} = defineProps<{ question: Question }>()
 </script>
