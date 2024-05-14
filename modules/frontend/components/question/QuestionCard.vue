4c2384be4640fee19f8be73c561d7536288f4791
alex1431999 <alexhaller99@yahoo.de>
2024-05-14 20:47:26 +0200

    feat: start sample page

---
+++ modules/frontend/components/question/QuestionCard.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-card>
+        {{ question }}
+    </v-card>
+</template>
+
+<script setup lang="ts">
+import type {Question} from 'fishing-academy-types'
+
+const {question} = defineProps<{ question: Question }>()
+</script>
---
+++ modules/frontend/pages/Question.vue
@@ -0,0 +1,15 @@
+<template>
+    <QuestionCard :question="sampleQuestion"></QuestionCard>
+</template>
+
+<script setup lang="ts">
+import type {Question, Choice} from 'fishing-academy-types'
+
+const sampleChoices: Choice[] = [{id: '1', description: 'a'}, {id: '2', description: 'b'}]
+const sampleQuestion: Question = {
+    id: 'test',
+    choices: sampleChoices,
+    answer: sampleChoices[0],
+    description: 'test question'
+}
+</script>
