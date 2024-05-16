1ef63de4d08469e135f0f5ede75e119282e25343
alex1431999 <alexhaller99@yahoo.de>
2024-05-16 21:37:06 +0200

    feat: make logo route to home

--- modules/frontend/components/header/AppHeader.vue
+++ modules/frontend/components/header/AppHeader.vue
@@ -1,7 +1,15 @@
 <template>
     <v-app-bar elevation="2">
         <template #prepend>
-            <img alt="company logo" src="/logo/svg/logo-no-background.svg">
+            <div class="logo ml-2" @click="$router.push('/')">
+                <img alt="company logo" src="/logo/svg/logo-no-background.svg">
+            </div>
         </template>
     </v-app-bar>
 </template>
+
+<style scoped>
+.logo {
+    cursor: pointer;
+}
+</style>
