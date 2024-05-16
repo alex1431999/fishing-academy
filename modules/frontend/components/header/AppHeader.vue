1be2082e920d52a12de2def848b031a7a9a79227
alex1431999 <alexhaller99@yahoo.de>
2024-05-16 21:35:17 +0200

    fix: update logo colors and include in header

--- modules/frontend/components/header/AppHeader.vue
+++ modules/frontend/components/header/AppHeader.vue
@@ -1,3 +1,7 @@
 <template>
-    <v-app-bar elevation="2"></v-app-bar>
+    <v-app-bar elevation="2">
+        <template #prepend>
+            <img alt="company logo" src="/logo/svg/logo-no-background.svg">
+        </template>
+    </v-app-bar>
 </template>
